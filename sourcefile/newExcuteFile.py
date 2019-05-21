c9d25cbeedc73b2ff8cf7319b01d5d25b69b8afe
doujiao <1129103770@qq.com>
2019-05-21 21:51:31 +0800

    modify newExcuteFile.py

--- sourcefile/newExcuteFile.py
+++ sourcefile/newExcuteFile.py
@@ -49,8 +49,55 @@ def plus_separately(fn,stoplist):
         tmp = cc
     return res
 
-# main function
-if __name__ == '__main__':
+impact_arr = ["very low", "low", "low", "low", "high", "high", "high", "fairly low", "high", "absolutely low", "fairly high", "fairly low", "fairly low"]
+
+
+
+
+
+def compute_risk_level(choice_arr):
+    f = []
+    out = []
+    for counter in range(len(choice_arr)):
+        impact_fn = init_fuzzy(impact_arr[counter])
+        fn_element = num_multiple_fuzzy(choice_arr[counter], impact_fn)
+        f.append(fn_element)
+        fns_of_risks = plus_separately(f,stops)
+        # 这里获取到的totalRes只有所有风险的模糊风险值，按顺序存放，没有风险名称
+        totalRes = fns_of_risks
+        
+    # 依次计算每一类风险的风险等级
+    # for index, totalRes in enumerate(fns_of_risks):
+    for (index, i) in enumerate(totalRes):
+        # print i[0]
+        sims1 = []
+        target_fn = FuzzyNumber.fnWithHeight(i, 1)
+        for key in score_levels.keys():
+            # 计算当前风险和所有等级的相似度
+            level_fn = FuzzyNumber.fnWithHeight(score_levels[key], 1)
+            sims1.append([key, FuzzyNumber.simlarity_caculator9(target_fn, level_fn)])
+
+        # 获取最高相似度
+        max_sim1 = max(sims1, key=lambda sim1: sim1[1])
+        sorted_sims1 = sorted(sims1, key=lambda sim1: sim1[1])
+
+        # 最高相似度对应的级别
+        risk_level1 = max_sim1[0]
+        # print index+1,Risk_names[index]
+
+        # y.append 传给前端[[x1,x2,x3...],[y1,y2,y3...]] x为风险类型，是固定的
+        # print risk_level1
+        out.append(risk_level1)
+
+        return out
+
+        # alarm_levels = ['fairly high','high','very high','absolutely high']
+        # if risk_level1 in alarm_levels:
+        #     # 生成风险报告
+        #     with open('')
+
+
+def test():
     # 依次读取choice和impact
     counter = 1
     f = []
@@ -82,13 +129,20 @@ if __name__ == '__main__':
 
         # 最高相似度对应的级别
         risk_level1 = max_sim1[0]
-        print index+1,Risk_names[index]
+        # print index+1,Risk_names[index]
 
         # y.append 传给前端[[x1,x2,x3...],[y1,y2,y3...]] x为风险类型，是固定的
-        print risk_level1
+        # print risk_level1
         out.append(risk_level1)
+
+        return out
 
         # alarm_levels = ['fairly high','high','very high','absolutely high']
         # if risk_level1 in alarm_levels:
         #     # 生成风险报告
         #     with open('')
+
+
+# main function
+if __name__ == '__main__':
+    test()
